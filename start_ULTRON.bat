6602c4f8194cec5c6101939b24bba74b12cea131
TAGORE <tagoreworld@gmail.com>
2026-08-03 23:39:08 +0530

    Rename start_jarvis.bat to start_ULTRON.bat

---
+++ start_ULTRON.bat
@@ -0,0 +1,4 @@
+@echo off
+timeout /t 45
+cd /d "C:\Users\tagor\Downloads\VoiceAssistant"
+"C:\Users\tagor\AppData\Local\Python\pythoncore-3.12-64\pythonw.exe" main.py
--- start_jarvis.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-timeout /t 45
-cd /d "C:\Users\tagor\Downloads\VoiceAssistant"
-"C:\Users\tagor\AppData\Local\Python\pythoncore-3.12-64\pythonw.exe" main.py
